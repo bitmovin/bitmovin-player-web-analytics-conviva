5d8a3afe6efae6dcb8be42552e9965aa8c04df33
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-03-02 14:53:35 +0100

    Export analytics into global namespace

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,8 @@
-console.log('hello world!');
+import {ConvivaAnalytics} from './ConvivaAnalytics';
+
+// Export Conviva Analytics to global namespace
+let w = (window as any);
+w.bitmovin = w.bitmovin || {};
+w.bitmovin.player = w.bitmovin.player || {};
+w.bitmovin.player.analytics = w.bitmovin.player.analytics || {};
+w.bitmovin.player.analytics.ConvivaAnalytics = ConvivaAnalytics;
