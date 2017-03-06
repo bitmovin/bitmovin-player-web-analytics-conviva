8c846ebeaa15755765008a5f9e9824e78258fc16
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-03-06 12:00:10 +0100

    Fix lint errors

--- src/ts/ConvivaAnalytics.ts
+++ src/ts/ConvivaAnalytics.ts
@@ -118,7 +118,7 @@ export class ConvivaAnalytics {
     contentMetadata.applicationName = this.config.applicationName || 'Unknown (no config.applicationName set)';
     contentMetadata.duration = this.player.getDuration(); // TODO how to handle HLS Chrome deferred duration detection?
     contentMetadata.streamType = this.player.isLive() ? Conviva.ContentMetadata.StreamType.LIVE // TODO how to handle HLS deferred live detection?
-        : Conviva.ContentMetadata.StreamType.VOD;
+      : Conviva.ContentMetadata.StreamType.VOD;
     contentMetadata.streamUrl = this.getUrlFromSource(source);
     contentMetadata.custom = {
       'playerType': this.player.getPlayerType(),
@@ -136,7 +136,7 @@ export class ConvivaAnalytics {
       this.logger.consoleLog('Something went wrong, could not obtain session key',
         Conviva.SystemSettings.LogLevel.ERROR);
     }
-    
+
     this.client.attachPlayer(this.sessionKey, this.playerStateManager);
   };
 
@@ -148,9 +148,9 @@ export class ConvivaAnalytics {
       playerState = Conviva.PlayerStateManager.PlayerState.STOPPED;
     } else if (this.player.isStalled()) {
       playerState = Conviva.PlayerStateManager.PlayerState.BUFFERING;
-    } else if(this.player.isPlaying()) {
+    } else if (this.player.isPlaying()) {
       playerState = Conviva.PlayerStateManager.PlayerState.PLAYING;
-    } else if(this.player.isPaused()) {
+    } else if (this.player.isPaused()) {
       playerState = Conviva.PlayerStateManager.PlayerState.PAUSED;
     }
 
@@ -177,7 +177,7 @@ export class ConvivaAnalytics {
       for (let key in object) {
         if (object.hasOwnProperty(key)) {
           let value = object[key];
-          if(typeof value === 'object') {
+          if (typeof value === 'object') {
             objectWalker(value, prefix + key + '.');
           } else {
             eventAttributes[prefix + key] = String(value);
@@ -191,7 +191,8 @@ export class ConvivaAnalytics {
   };
 
   private reportError = (event: any) => {
-    this.client.reportError(this.sessionKey, String(event.code) + ' ' + event.message, Conviva.Client.ErrorSeverity.FATAL);
+    this.client.reportError(this.sessionKey, String(event.code) + ' ' + event.message,
+      Conviva.Client.ErrorSeverity.FATAL);
     this.endSession();
   };
 
@@ -245,7 +246,7 @@ export class ConvivaAnalytics {
    */
   sendCustomPlaybackEvent(eventName: string, eventAttributes: EventAttributes = {}): void {
     // Check for active session
-    if (this.sessionKey == Conviva.Client.NO_SESSION_KEY) {
+    if (this.sessionKey === Conviva.Client.NO_SESSION_KEY) {
       this.logger.consoleLog('cannot send playback event, no active monitoring session',
         Conviva.SystemSettings.LogLevel.WARNING);
       return;
