387d98ebb2445dc04eb2d46b7fea521157bb3a74
Fabian Pirklbauer <fabian.pirklbauer@bitmovin.com>
2018-12-12 16:25:23 +0100

    Switch to webpack and yarn

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/ts/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bitmovinplayer-analytics-conviva.js',
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      root: 'ConvivaAnalytics',
+      amd: 'bitmovinplayer-analytics-conviva',
+      commonjs: 'bitmovinplayer-analytics-conviva'
+    },
+    libraryTarget: 'umd'
+  }
+};
