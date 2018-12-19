b7d3904250d6fcdb76837281b82ef5dc16bdac2c
Fabian Pirklbauer <fabian.pirklbauer@bitmovin.com>
2018-12-19 10:53:12 +0100

    Remove whitespace

--- src/ts/Html5Timer.ts
+++ src/ts/Html5Timer.ts
@@ -5,7 +5,7 @@ export class Html5Timer implements Conviva.TimerInterface {
 
   public createTimer(timerAction: TimerAction, intervalMs: number, actionName?: string | null): TimerCancelFunction {
     let timerId = setInterval(timerAction, intervalMs);
-    return (function () {
+    return (function() {
       if (timerId !== -1) {
         clearInterval(timerId);
         timerId = -1;
