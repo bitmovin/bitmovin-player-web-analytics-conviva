3e683874625b6646de36de83c84331b5bee2eea2
David Steinacher <david.steinacher@bitmovin.com>
2019-02-27 12:45:27 +0100

    extract expected session key for content and ad

--- spec/helper/TestsHelper.ts
+++ spec/helper/TestsHelper.ts
@@ -1,6 +1,9 @@
 import { BrowserUtils} from '../../src/ts/helper/BrowserUtils';
 jest.mock('../../src/ts/helper/BrowserUtils');
 
+export const CONTENT_SESSION_KEY: number = 0;
+export const AD_SESSION_KEY: number = 1;
+
 beforeAll(() => {
   BrowserUtils.isMobile = jest.fn().mockReturnValue(false);
 });
