c6997bf340d52cdb7bf65d320fc826a7a65c684a
David Steinacher <david.steinacher@bitmovin.com>
2019-02-27 13:25:00 +0100

    rename UNKNOWN_VALUE_KEY to UNKNOWN_VALUE

--- src/ts/AdInsightsTrackingPlugin.ts
+++ src/ts/AdInsightsTrackingPlugin.ts
@@ -5,7 +5,7 @@ import { EventAttributes } from './ConvivaAnalytics';
 // TODO: Description
 export class AdInsightsTrackingPlugin extends AdBreakTrackingPlugin {
 
-  private static UNKNOWN_VALUE_KEY = 'NA';
+  private static UNKNOWN_VALUE = 'NA';
 
   private adSessionKey: number = Conviva.Client.NO_SESSION_KEY;
   private adPlayerStateManager: Conviva.PlayerStateManager;
@@ -28,20 +28,20 @@ export class AdInsightsTrackingPlugin extends AdBreakTrackingPlugin {
 
     // Create a new ContentMetadata object for ad.
     let adMetadata = new Conviva.ContentMetadata();
-    adMetadata.assetName = adData.adTitle || AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY;
+    adMetadata.assetName = adData.adTitle || AdInsightsTrackingPlugin.UNKNOWN_VALUE;
     adMetadata.streamUrl = ad.mediaFileUrl;
     adMetadata.duration = ad.duration; // Ad Duration In seconds
 
     // custom takes a javascript object as an argument
     adMetadata.custom = {
       // Required
-      'c3.ad.technology': AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY, // TODO: support server side in case of yospace
-      'c3.ad.id': ad.id || AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY,
-      'c3.ad.system': adData && adData.adSystem && adData.adSystem.name || AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY,
+      'c3.ad.technology': AdInsightsTrackingPlugin.UNKNOWN_VALUE, // TODO: support server side in case of yospace
+      'c3.ad.id': ad.id || AdInsightsTrackingPlugin.UNKNOWN_VALUE,
+      'c3.ad.system': adData && adData.adSystem && adData.adSystem.name || AdInsightsTrackingPlugin.UNKNOWN_VALUE,
       'c3.ad.position': String(this.currentAdBreakPosition),
-      'c3.ad.type': AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY,
-      'c3.ad.mediaFileApiFramework': adData && adData.apiFramework || AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY,
-      'c3.ad.adStitcher': AdInsightsTrackingPlugin.UNKNOWN_VALUE_KEY, // TODO: find a way to export that from the yospace integration
+      'c3.ad.type': AdInsightsTrackingPlugin.UNKNOWN_VALUE,
+      'c3.ad.mediaFileApiFramework': adData && adData.apiFramework || AdInsightsTrackingPlugin.UNKNOWN_VALUE,
+      'c3.ad.adStitcher': AdInsightsTrackingPlugin.UNKNOWN_VALUE, // TODO: find a way to export that from the yospace integration
     };
 
     const addValueIfPresent = (key: string, value: string) => {
