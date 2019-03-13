961f7cb4bd7b891aa139ccc30542f4b6bcb66e0a
David Steinacher <david.steinacher@bitmovin.com>
2019-03-13 09:56:32 +0100

    remove duplicate comment

--- src/ts/ConvivaAnalytics.ts
+++ src/ts/ConvivaAnalytics.ts
@@ -546,8 +546,6 @@ export class ConvivaAnalytics {
   };
 
   private onVideoQualityChanged = (event: VideoQualityChangedEvent) => {
-    // We calculate the bitrate with a divisor of 1000 so the values look nicer
-    // Example: 250000 / 1000 => 250 kbps (250000 / 1024 => 244kbps)
     const bitrateKbps = BitrateHelper.calculateKbps(event.targetQuality.bitrate);
 
     if (!this.isSessionActive()) {
