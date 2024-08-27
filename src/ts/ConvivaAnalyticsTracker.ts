aaef75b2fcd850375d4e2f257f4e47696993e27e
Kirill Karpov <karpov.kir.v@gmail.com>
2024-08-27 15:02:01 +0400

    Streamline invocation order

--- src/ts/ConvivaAnalyticsTracker.ts
+++ src/ts/ConvivaAnalyticsTracker.ts
@@ -483,6 +483,11 @@ export class ConvivaAnalyticsTracker {
 
     this.debugLog('[ ConvivaAnalyticsTracker ] new session key', this.sessionKey);
 
+    this.setPlayerInfo();
+    // It's required to correctly track VST. There must be BUFFERING or STOPPED metric reported before PLAYING.
+    // In some cases BUFFERING does not fire before PLAYING, so we report STOPPED right after session initialization to cover all edge cases.
+    this.convivaVideoAnalytics.reportPlaybackMetric(Conviva.Constants.Playback.PLAYER_STATE, Conviva.Constants.PlayerState.STOPPED);
+
     this.convivaVideoAnalytics.setCallback(() => {
       if (!this.isPlayerAttached) {
         return;
@@ -512,11 +517,6 @@ export class ConvivaAnalyticsTracker {
         Conviva.SystemSettings.LogLevel.ERROR,
       );
     }
-
-    this.setPlayerInfo();
-    // It's required to correctly track VST. There must be BUFFERING or STOPPED metric reported before PLAYING.
-    // In some cases BUFFERING does not fire before PLAYING, so we report STOPPED right after session initialization to cover all edge cases.
-    this.convivaVideoAnalytics.reportPlaybackMetric(Conviva.Constants.Playback.PLAYER_STATE, Conviva.Constants.PlayerState.STOPPED);
   }
 
   /**
